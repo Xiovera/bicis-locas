6b0623893b7d725423344d47fa6bfbbcda855985
Rocio Yovera <rocioyovera@ gmail.com>
2017-01-13 17:14:32 -0500

    punto uno ok

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var username = document.getElementById("name").value;
+	var userlastname = document.getElementById("lastname").value;
+	var useremail = document.getElementById("input-email").value;
+	var userpassword = document.getElementById("input-password").value;
+
+	if(username ==""){
+		alert("Por favor digite su nombre");
+		return false;
+	}	
+
+	if(userlastname ==""){
+		alert("Por favor digite su apellido");
+		return false;
+	}	
+
+	if(useremail ==""){
+		alert("Por favor digite su correo electrónico");
+		return false;
+	}	
+
+	if(userpassword ==""){
+		alert("Por favor digite su contraseña");
+		return false;
+	}
 }
